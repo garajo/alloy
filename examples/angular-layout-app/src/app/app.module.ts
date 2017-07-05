e0443a3a2dc7301c6d912f388a0787984c0aa161
Roman Minkin <roman.minkin@keysight.com>
2017-07-05 13:49:11 -0400

    Cleaning up and refactoring

--- examples/angular-layout-app/src/app/app.module.ts
+++ examples/angular-layout-app/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { DropdownModule } from '@ksf/alloy'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DropdownsPageComponent } from './components/dropdowns-page/dropdowns-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropdownsPageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ examples/angular-layout-app/src/app/components/dropdowns-page/dropdowns-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dropdowns-page',
+  templateUrl: './dropdowns-page.component.html',
+  styleUrls: ['./dropdowns-page.component.scss']
+})
+export class DropdownsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
