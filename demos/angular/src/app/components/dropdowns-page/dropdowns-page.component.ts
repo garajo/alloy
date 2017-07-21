3f0f3d1f828f3ff217bc082d93085cfd2187aa29
Roman Minkin <roman.minkin@keysight.com>
2017-07-21 12:02:38 -0400

    Extend demo for dropdown Fix checkmark for selected option for dropdown

--- demos/angular/src/app/components/dropdowns-page/dropdowns-page.component.ts
+++ demos/angular/src/app/components/dropdowns-page/dropdowns-page.component.ts
@@ -6,15 +6,35 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./dropdowns-page.component.scss']
 })
 export class DropdownsPageComponent implements OnInit {
-    selectedOption: string;
+    private id: number = 0;
+
+    selectedValue: string;
+    placeholder: string = 'Select One';
 
 
     modules = [
-        { value: 'qpsk-0', name: 'QPSK' },
-        { value: 'lmr-1', name: 'LMR' },
-        { value: 'some-module-3', name: 'Some module' }
+        { value: 'wf-0', name: 'Waveform Filename' },
+        { value: 'wl-1', name: 'Waveform Length' },
+        { value: 'cf-2', name: 'Carrier Frequency' }
     ];
 
+
+    add() {
+        this.modules.push({value: `${this.id}`, name: `Name ${this.id}`});
+        this.id++;
+    }
+
+    remove(_module) {
+        const id: number = this.modules.findIndex((m, i) => {
+            return this.modules[i].value === _module.value;
+        });
+
+        console.log('id', id);
+
+        if (id >=0) {
+            this.modules.splice(id, 1);
+        }
+    }
 
     constructor() { }
 
